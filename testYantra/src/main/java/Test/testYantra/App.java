e9a104094148df4a4af8bd1ec1356b293c13b392
pc <pc@DESKTOP-0LO9GIC>
2021-12-11 11:39:14 +0530

    done

---
+++ testYantra/src/main/java/Test/testYantra/App.java
@@ -0,0 +1,13 @@
+package Test.testYantra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
